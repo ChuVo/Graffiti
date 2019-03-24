0318e8d5fac4982a3087ec03621f1fad8b744b53
ChuVo <cahek764@ya.ru>
2019-03-24 11:12:17 +0300

    finished verstka

--- js/section-order.js
+++ js/section-order.js
@@ -1,5 +1,10 @@
 (function() {
-  const elems = document.querySelectorAll('.patterns__items');
+  const elems = document.querySelectorAll('.patterns__items'),
+        arrowListSize = document.querySelector('.list-size__arrow'),
+        listSize = document.querySelector('.list-size');
+
+  arrowListSize.addEventListener( 'click', showListSize );
+  listSize.addEventListener( 'click', printSize );
 
   let array = [],
       index = undefined;
@@ -28,8 +33,7 @@
   function getIndexMouseOut() {
     array.forEach( (i) => {
       i.addEventListener( 'mouseout', () => {        
-        index = array.indexOf(i);
-        console.log( index + ' = index');        
+        index = array.indexOf(i);       
 
         reChangeColor(index);
       });
@@ -38,9 +42,23 @@
   
   function reChangeColor(index) {
     index +=2;
-    console.log( index + ' = color');
     document.querySelector('.patterns__items:nth-child(' + index + ') .patterns__item').classList.remove('patterns__item--color');
   }
+
+  function showListSize() {    
+      listSize.classList.toggle('list-size--visible');
+  }
+
+  function printSize(event) {
+    const sizeArea = document.querySelector('.put__subtitle');
+
+    let sizeValue = event.target.innerHTML;
+
+    sizeArea.innerHTML = sizeValue;
+    showListSize();
+  }
+  
+
   
 console.log( getIndexMouseOver() );
 console.log( getIndexMouseOut() );
